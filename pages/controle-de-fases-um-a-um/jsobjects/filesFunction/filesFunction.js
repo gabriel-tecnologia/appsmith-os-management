8492550b5a604c550c258d0838fb48b4d281c422
rafael.vaz <rafael.vaz@gabriel.com.br>
2025-05-12 16:26:33 +0000

    feat: new query logic to upload images one by one

---
+++ pages/controle-de-fases-um-a-um/jsobjects/filesFunction/filesFunction.js
@@ -0,0 +1,276 @@
+export default {
+	
+	async compressAndUpload(files) {
+		const scale = 0.5;
+    const quality = 0.9;
+		
+		let finalCompressedFile;
+    
+		for (const file of files) {
+			console.log("Iniciando processamento do arquivo:", file.name);
+
+			// Se for vídeo, adiciona diretamente à lista
+			if (file.type === "video/mp4") {
+				try {
+					showAlert(`Arquivo de vídeo '${file.name}' enviado sem compressão`, 'success')
+					await this.envia_arquivo_pra_nuvem(file)
+					console.log("Arquivo de vídeo bypassado:", file.name);
+				}
+				catch(error) {
+					showAlert(`Falha ao enviar o arquivo de vídeo '${file.name}'`, 'error')
+				}
+				continue;
+
+			}
+
+			try {
+				// Convertendo de base64 para Blob
+				const fileBlob = await this.base64ToBlob(file.data);
+				console.log("Blob criado para o arquivo:", file.name);
+
+				// Comprimindo a imagem
+				const compressedFile = await this.compressImage(fileBlob, scale, quality, file.name);
+				console.log("Imagem comprimida:", file.name);
+
+				// Convertendo de Blob para Base64
+				const compressedBase64 = await this.blobToBase64(compressedFile);
+				console.log("Imagem convertida para Base64:", file.name);
+
+				// Formatando
+				finalCompressedFile = {
+					data: compressedBase64,
+					id: file.id,
+					name: file.name,
+					type: file.type,
+					size: compressedFile.size
+				};
+				
+				showAlert(`Arquivo '${finalCompressedFile.name}' foi comprimido com sucesso`, "success");
+
+			} catch (error) {
+				console.error("Erro ao processar o arquivo:", file.name, error);
+				showAlert(`Falha ao comprimir o arquivo '${finalCompressedFile.name}'`, "error")
+				await this.envia_arquivo_pra_nuvem(file); // Envia os arquivos originais, se falhar
+			}
+		
+			try {
+					console.log("Iniciando upload de arquivos comprimidos...");
+					await this.envia_arquivo_pra_nuvem(finalCompressedFile);
+					showAlert(`Upload do arquivo '${finalCompressedFile.name}' finalizado com sucesso`, "success");
+			} catch (error) {
+					console.error("Erro ao fazer upload do arquivo", error);
+					showAlert(`Falha ao fazer upload o arquivo '${finalCompressedFile.name}'`, "error")
+			}
+		} 
+	},
+	
+	async compressImage(file, scale, quality, fileName) {
+		const options = {
+			maxSizeMB: 1,
+			maxWidthOrHeight: Math.round(2000 * scale),
+			useWebWorker: true,
+			initialQuality: quality
+		};
+
+		try {
+			const compressedBlob = await imageCompression(file, options);
+			return this.blobToFile(compressedBlob, fileName);
+		} catch (error) {
+			console.error('Erro ao comprimir a imagem:', error);
+			throw error;
+		}
+	},
+		
+	async base64ToBlob(dataURL) {
+    console.log("DataURL recebido em base64ToBlob", dataURL);
+		
+    const response = await fetch(dataURL);
+    const blob = await response.blob();
+		
+    console.log("Blob enviado como resposta", blob);
+		
+    return blob;
+	},
+	
+	blobToFile(blob, fileName) {
+		const object = {
+			name: fileName,
+			type: blob.type,
+			size: blob.size,
+			blob: blob
+		};
+		return object
+	},
+	
+	async blobToBase64(input) {
+		const blob = input.blob || input;
+
+    // Valida se o objeto é um Blob válido
+    if (!blob || typeof blob.arrayBuffer !== "function") {
+        console.error("Objeto passado não é um Blob válido:", blob);
+        throw new Error("Objeto não é um Blob válido");
+    }
+
+    console.log("Iniciando conversão de Blob para Base64, tamanho:", blob.size);
+
+    try {
+        // Converte o Blob em ArrayBuffer
+        const arrayBuffer = await blob.arrayBuffer();
+        const uint8Array = new Uint8Array(arrayBuffer);
+
+        // Processa o ArrayBuffer em pedaços menores
+        const chunkSize = 1024 * 1024; // 1 MB
+        let binaryString = "";
+
+        for (let i = 0; i < uint8Array.length; i += chunkSize) {
+            const chunk = uint8Array.slice(i, i + chunkSize);
+            binaryString += Array.from(chunk, byte => String.fromCharCode(byte)).join("");
+        }
+
+        // Converte a string binária para Base64
+        const base64String = btoa(binaryString);
+        return `data:${blob.type};base64,${base64String}`;
+    } catch (error) {
+        console.error("Erro ao converter Blob para Base64:", error);
+        throw new Error("Erro durante a conversão de Blob para Base64");
+    }
+	},
+	
+	async envia_arquivo_pra_nuvem(arquivo) {
+		storeValue("tipo_arquivo", "Foto do Servico")
+		let arquivos_para_envio;
+		let fotos = appsmith.store.selectedOS["Foto do Serviço"]
+		console.log(fotos)
+		if(fotos == undefined){
+			arquivos_para_envio = [];
+		}
+		else{
+			arquivos_para_envio = fotos.map(foto => ({"url": foto.url}));
+		}
+		
+		console.log(arquivos_para_envio)
+		
+		try {
+			const resposta = await Enviar_Arquivo_S3.run({
+				fileName: arquivo.name,
+				filesData: arquivo
+			});
+			const url = resposta.signedUrl;
+			arquivos_para_envio.push({"url": url});
+			storeValue('arquivo_para_nuvem', null);
+			showAlert(`Arquivo '${arquivo.name}' enviado ao S3 com sucesso`, "success");
+		}
+		catch(error) {
+			showAlert(`Falha ao enviar o arquivo '${arquivo.name}' ao S3`, "error");
+		}
+		
+		try {
+			await Enviar_Fotos_Airtable.run({
+				photosUrl: arquivos_para_envio
+			});
+			showAlert(`Arquivo '${arquivo.name}' enviado ao Airtable com sucesso`, "success");
+		}
+		catch(error) {
+			showAlert(`Falha ao enviar o arquivo '${arquivo.name}' ao Airtable`, "error");
+		}
+
+		storeValue('arquivo_para_nuvem', null);
+		storeValue('arquivos_para_envio_airtable', null);
+		
+		const newOS = await Leitura_OS_Por_RecordID.run({
+			recordId: appsmith.store.selectedOS.record_id
+		});
+		storeValue('selectedOS', newOS.fields)
+		
+		galery.model.data = newOS.fields["Foto do Serviço"]
+		
+		resetWidget("listaVideos", true)
+	},
+	
+	async enviarTermoS3(arquivos) {
+		storeValue("tipo_arquivo", "term")
+		let arquivos_para_envio = [];
+		
+		for (const arquivo of arquivos){
+			storeValue('arquivo_para_nuvem', arquivo);
+			const resposta = await Enviar_Arquivo_S3.run({
+				filesData: arquivo
+			});
+			const url = resposta.signedUrl;
+			arquivos_para_envio.push({"url": url});
+			storeValue('arquivo_para_nuvem', null);
+		}
+		
+		try {
+			await Enviar_Termo.run({
+				term: arquivos_para_envio
+			});
+			showAlert("Termo enviado com sucesso", "success")
+		}
+		catch (error) {
+			showAlert("Falha ao enviar termo de finalização", "error")
+		}
+
+		storeValue('arquivo_para_nuvem', null);
+		storeValue('arquivos_para_envio_airtable', null);
+		
+		const newOS = await Leitura_OS_Por_RecordID.run({
+			recordId: appsmith.store.selectedOS.record_id
+		});
+		storeValue('selectedOS', newOS.fields)
+	},
+	
+	async removerFotoAirtable() {
+		let fotos = appsmith.store.selectedOS["Foto do Serviço"]
+		
+		fotos = fotos.filter(image => image.url != galery.model.image.url)
+				
+		try {
+			await Enviar_Fotos_Airtable.run({
+				photosUrl: fotos
+			});
+			const newOS = await Leitura_OS_Por_RecordID.run({
+				recordId: appsmith.store.selectedOS.record_id
+			});
+			storeValue('selectedOS', newOS.fields)
+			resetWidget("listaVideos", true)
+			galery.model.data = newOS.fields["Foto do Serviço"]
+			showAlert(`Imagem removida com sucesso '${galery.model.image.filename}'`, "success")
+		}
+		catch(error) {
+			showAlert("Falha ao remover arquivo", "error")
+		}
+	},
+	
+	async removerVideoAirtable(video) {
+		let arquivos = appsmith.store.selectedOS["Foto do Serviço"]
+		
+		arquivos = arquivos.filter(arquivo => arquivo.url != video.url)
+		
+		try {
+			await Enviar_Fotos_Airtable.run({
+				photosUrl: arquivos
+			});
+			const newOS = await Leitura_OS_Por_RecordID.run({
+				recordId: appsmith.store.selectedOS.record_id
+			});
+			storeValue('selectedOS', newOS.fields)
+			showAlert(`Vídeo removido com sucesso '${video.filename}'`, "success")
+		}
+		catch(error) {
+			showAlert("Falha ao remover arquivo", "error")
+		}
+	},
+	
+	renderVideos(list) {
+		return list.filter(file => 
+			(file.filename && file.filename.includes("mp4")) || 
+			(file.url && file.url.includes("mp4")) || 
+			(file.type && file.type.includes("mp4"))
+		);
+	},
+	
+	renderImagens(list) {
+		return list.filter(file => file.type == "image/jpeg" || file.type == "image/png")
+	}
+}
---
+++ pages/controle-de-fases-um-a-um/jsobjects/renderFunctions/renderFunctions.js
@@ -0,0 +1,474 @@
+export default {
+	renderPhaseState(){
+		switch(appsmith.store.selectedOS["Fase"]){
+			case "Agendamento de Serviço":
+				return "<span style='color: #00ce7c'>Agendamento ➞ </span><span style='color: #858282'>Fila ➞ Em Andamento ➞ Controle de Qualidade  ⇶ Destino OS</span>"
+			case "Reagendamento de Serviço":
+				return "<span style='color: #00ce7c'>Reagendamento ➞ </span><span style='color: #858282'>Fila ➞ Em Andamento ➞ Controle de Qualidade  ⇶ Destino OS</span>"
+			case "Fila de Serviço":
+				return "<span style='color: #00ce7c'>Agendamento ➞ Fila ➞ </span> <span style='color: #858282'>Em Andamento ➞ Controle de Qualidade  ⇶ Destino OS</span>"
+			case "Serviço em Andamento":
+				return "<span style='color: #00ce7c'>Agendamento ➞ Fila ➞ Em Andamento ➞ </span> <span style='color: #858282'>Controle de Qualidade  ⇶ Destino OS</span>"
+			case "Controle de Qualidade":
+				return "<span style='color: #00ce7c'>Agendamento ➞ Fila ➞ Em Andamento ➞ Controle de Qualidade  ⇶ </span> <span style='color: #858282'>Destino OS</span>"
+			case "Concluído":
+				return "<span style='color: #00ce7c'>Agendamento ➞ Fila ➞ Em Andamento ➞ Controle de Qualidade ➞ Concluído </span>"
+			case "Improdutiva":
+				return "<span style='color: #00ce7c'>Agendamento ➞ Fila ➞ Em Andamento ➞ Controle de Qualidade ➞ </span> <span style='color: #ef4444'>Improdutiva</span>"
+			case "Ajuste":
+				return "<span style='color: #00ce7c'>Agendamento ➞ Fila ➞ Em Andamento ➞ Controle de Qualidade ➞ </span> <span style='color: #eab308'>Ajuste ⇉ </span> <span style='color: #858282'>Destino OS </span>"
+			case "Cancelado":
+				return "<span style='color: #00ce7c'>Agendamento ➞ Fila ➞ Em Andamento ➞ Controle de Qualidade ➞ </span> <span style='color: #ef4444'>Cancelado</span>"
+			default:
+				return "Agendamento ➞ Fila ➞ Em Andamento ➞ Controle de Qualidade ➞ Concluido"
+		}
+	},
+	renderReasons(){
+		switch(appsmith.store.changeTo){
+			case "Ajuste":
+				return this.renderAdjustReasons.data
+			case "Improdutiva":
+				return this.renderImproductiveReasons.data
+			default:
+				return []
+		}
+	},
+	
+	renderAdjustReasons(){
+		return [
+								{
+									"name": "Termo de finalização não assinado",
+									"code": "Termo de finalização não assinado"
+								},
+								{
+									"name": "Controle de Qualidade Sistêmico Reprovado",
+									"code": "Controle de Qualidade Sistêmico Reprovado"
+								}
+							]
+	},
+	renderImproductiveReasons(){
+		return [
+			{
+				"name": "[CLIENTE] Sem ponto elétrico disponível",
+				"code": "[CLIENTE] Sem ponto elétrico disponível"
+			},
+			{
+				"name": "[CLIENTE] Cliente ou síndico indisponível",
+				"code": "[CLIENTE] Cliente ou síndico indisponível"
+			},
+			{
+				"name": "[CLIENTE] Cliente ou síndico não ciente",
+				"code": "[CLIENTE] Cliente ou síndico não ciente"
+			},
+			{
+				"name": "[CLIENTE] Não aceitou infraestrutura",
+				"code": "[CLIENTE] Não aceitou infraestrutura"
+			},
+			{
+				"name": "[CLIENTE] Decisões ou preferências que exigem reagendamento (ex.: infra embutida, mudança de local, etc.)",
+				"code": "[CLIENTE] Decisões ou preferências que exigem reagendamento (ex.: infra embutida, mudança de local, etc.)"
+			},
+			{
+				"name": "[CLIENTE] Local em obra",
+				"code": "[CLIENTE] Local em obra"
+			},
+			{
+				"name": "[PARCEIRO] Atraso não tolerado",
+				"code": "[PARCEIRO] Atraso não tolerado"
+			},
+			{
+				"name": "[PARCEIRO] Problemas com veículos ou materiais insuficientes",
+				"code": "[PARCEIRO] Problemas com veículos ou materiais insuficientes"
+			},
+			{
+				"name": "[PARCEIRO] Controle de qualidade físico reprovado (ex.angulação inadequada, sealtubo mal instalado...)",
+				"code": "[PARCEIRO] Controle de qualidade físico reprovado (ex.angulação inadequada, sealtubo mal instalado...)"
+			},
+			{
+				"name": "[PARCEIRO] Falta de evidências fotográficas",
+				"code": "[PARCEIRO] Falta de evidências fotográficas"
+			},
+			{
+				"name": "[GABRIEL] Sem sinal de 4G",
+				"code": "[GABRIEL] Sem sinal de 4G"
+			},
+			{
+				"name": "[GABRIEL] Necessidade de cabeamento de internet",
+				"code": "[GABRIEL] Necessidade de cabeamento de internet"
+			},
+			{
+				"name": "[GABRIEL] Equipamento com defeito (SN/Camaleão)",
+				"code": "[GABRIEL] Equipamento com defeito (SN/Camaleão)"
+			},
+			{
+				"name": "[EXTERNO] Chuva ou condições climáticas adversas",
+				"code": "[EXTERNO] Chuva ou condições climáticas adversas"
+			},
+			{
+				"name": "Não foi necessário realizar o serviço",
+				"code": "Não foi necessário realizar o serviço"
+			}										
+		]	
+	},
+	
+	renderReasonsOptions(){
+		if (appsmith.store.improductiveMotive) {
+			return appsmith.store.improductiveMotive
+		}
+		else if (appsmith.store.adjustMotive) {
+			return appsmith.store.adjustMotive
+		}
+		else {
+			return []
+		}
+	},
+	renderTab(){
+		switch(appsmith.store.selectedOS.Fase){
+			case "Controle de Qualidade":
+				return "Controle de Qualidade"
+			case "Ajuste":
+				return "Ajuste"
+			case "Serviço em Andamento":
+				return "Serviço em Andamento"
+			default:
+				return "Outros"
+		}
+	},
+	renderAvailablePartners() {
+		const PARTNER = [
+			"[EXT] C&K",
+			"[EXT] Multivoltz",
+			"[EXT] CAVI Tecnologia",
+			"[EXT] Icatel",
+			"[EXT] JLB SOLUÇÕES",
+			"[EXT] MDJ Solutions",
+			"[EXT] Power.com",
+			"[EXT] T7 Solutions",
+			"[EXT] Telebit"
+		]
+		
+		const partner = PARTNER.map((partner) =>({name: partner, code: partner}))		
+		return partner
+	},
+	
+	renderSolutions() {
+		return [
+			{"name": "Remoção ou troca de equipamento ou componente", "code": "Remoção ou troca de equipamento ou componente"},
+			{"name": "Reinicialização de equipamento", "code": "Reinicialização de equipamento"},
+			{"name": "Não se aplica", "code": "Não se aplica"},
+			{"name": "Restabelecimento automático", "code": "Restabelecimento automático"},
+			{"name": "Atualização, ajuste ou reconfiguração do Software/Firmware", "code": "Atualização, ajuste ou reconfiguração do Software/Firmware"},
+			{"name": "Readequação/reparo de infraestrutura", "code": "Readequação/reparo de infraestrutura"},
+			{"name": "Reinicialização do modem da Internet/conexão 4G", "code": "Reinicialização do modem da Internet/conexão 4G"},
+			{"name": "Solicitação de visita técnica da Operadora ao cliente", "code": "Solicitação de visita técnica da Operadora ao cliente"},
+			{"name": "Desobstrução, limpeza ou ajuste da câmera", "code": "Desobstrução, limpeza ou ajuste da câmera"},
+			{"name": "Remanejamento do rack/câmeras", "code": "Remanejamento do rack/câmeras"},
+			{"name": "Envio de placas da Gabriel", "code": "Envio de placas da Gabriel"}
+		]
+	},
+	
+	formatStockText(stock) {
+    let text = ""
+		
+		stock["Status da Remessa"] ? text = text.concat("Status: ", stock["Status da Remessa"], "\n\n") : ""
+		
+		stock["Saída de Material"] ? text = text.concat("Saída de Material: ", stock["Saída de Material"], "\n\n") : ""
+		
+		stock["Nota de Remessa / Retorno"] ? text = text.concat("Nota de Remessa / Retorno: ", stock["Nota de Remessa / Retorno"], "\n\n") : ""
+		
+		stock["Movimento Criado por"] ? text = text.concat("Movimento Criado por: ", stock["Movimento Criado por"], "\n\n") : ""
+		
+		stock.Camaleão ? text = text.concat(stock.Camaleão+" x Camaleão\n") : ""
+		
+		stock["Sistema Nervoso 2"] ? text = text.concat(stock["Sistema Nervoso 2"]+" x Sistema Nervoso 2\n") : ""
+		
+		stock["Kit Poste Canguru"] ? text = text.concat(stock["Kit Poste Canguru"] + " x Kit Poste Canguru\n") : ""
+		
+		stock["Kit Poste 2.0 (Piso)"] ? text = text.concat(stock["Kit Poste 2.0 (Piso)"] + " x Kit Poste 2.0 (Piso)\n") : ""
+		
+		stock["Kit Poste 2.0 (Parede)"] ? text = text.concat(stock["Kit Poste 2.0 (Parede)"]+" x Kit Poste 2.0 (Parede)\n") : ""
+		
+		stock["Placa de Sinalização - Gravata - 0800 GAB-RIEL"] ? text = text.concat(stock["Placa de Sinalização - Gravata - 0800 GAB-RIEL"]+" x Placa Gravata Gen1 (Verde)\n") : ""
+		
+		stock["Placa de Sinalização - Gravata 2- 0800 GAB-RIEL"] ? text = text.concat(stock["Placa de Sinalização - Gravata 2- 0800 GAB-RIEL"]+" x Placa Gravata Gen2 (Branca)\n") : ""
+		
+		stock["Placa Bolacha"] ? text = text.concat(stock["Placa Bolacha"]+" x Placa Bolacha Nova\n") : ""
+		
+		return text
+	},
+	
+	renderPeriod() {
+		switch (appsmith.store.selectedOS["Tipo de Ordem de Serviço"]){
+			case "Manutenção":
+				return [
+					{
+						"name": "Horário comercial",
+						"code": "Horário comercial"
+					},
+					{
+						"name": "Manhã",
+						"code": "Manhã"
+					},
+					{
+						"name": "08h",
+						"code": "08h"
+					},
+					{
+						"name": "09h",
+						"code": "09h"
+					},
+					{
+						"name": "10h",
+						"code": "10h"
+					},
+					{
+						"name": "11h",
+						"code": "11h"
+					},
+					{
+						"name": "Tarde",
+						"code": "Tarde"
+					},
+					{
+						"name": "12h",
+						"code": "12h"
+					},
+					{
+						"name": "13h",
+						"code": "13h"
+					},
+					{
+						"name": "14h",
+						"code": "14h"
+					},
+					{
+						"name": "15h",
+						"code": "15h"
+					},{
+						"name": "16h",
+						"code": "16h"
+					},
+					{
+						"name": "17h",
+						"code": "17h"
+					},
+					{
+						"name": "18h",
+						"code": "18h"
+					}
+				]
+				case "Desinstalação":
+				return [
+					{
+						"name": "Horário comercial",
+						"code": "Horário comercial"
+					},
+					{
+						"name": "Manhã",
+						"code": "Manhã"
+					},
+					{
+						"name": "08h",
+						"code": "08h"
+					},
+					{
+						"name": "09h",
+						"code": "09h"
+					},
+					{
+						"name": "10h",
+						"code": "10h"
+					},
+					{
+						"name": "11h",
+						"code": "11h"
+					},
+					{
+						"name": "Tarde",
+						"code": "Tarde"
+					},
+					{
+						"name": "12h",
+						"code": "12h"
+					},
+					{
+						"name": "13h",
+						"code": "13h"
+					},
+					{
+						"name": "14h",
+						"code": "14h"
+					},
+					{
+						"name": "15h",
+						"code": "15h"
+					},{
+						"name": "16h",
+						"code": "16h"
+					},
+					{
+						"name": "17h",
+						"code": "17h"
+					},
+					{
+						"name": "18h",
+						"code": "18h"
+					}
+				]
+			case "Aditivo":
+				return [
+					{
+						"name": "Horário comercial",
+						"code": "Horário comercial"
+					},
+					{
+						"name": "Manhã",
+						"code": "Manhã"
+					},
+					{
+						"name": "08h",
+						"code": "08h"
+					},
+					{
+						"name": "09h",
+						"code": "09h"
+					},
+					{
+						"name": "10h",
+						"code": "10h"
+					},
+					{
+						"name": "11h",
+						"code": "11h"
+					},
+					{
+						"name": "Tarde",
+						"code": "Tarde"
+					},
+					{
+						"name": "12h",
+						"code": "12h"
+					},
+					{
+						"name": "13h",
+						"code": "13h"
+					},
+					{
+						"name": "14h",
+						"code": "14h"
+					},
+					{
+						"name": "15h",
+						"code": "15h"
+					},
+					{
+						"name": "16h",
+						"code": "16h"
+					}
+				]
+				case "Adesão":
+					return [
+						{
+							"name": "Horário comercial",
+							"code": "Horário comercial"
+						},
+						{
+							"name": "Manhã",
+							"code": "Manhã"
+						},
+						{
+							"name": "08h",
+							"code": "08h"
+						},
+						{
+							"name": "09h",
+							"code": "09h"
+						},
+						{
+							"name": "10h",
+							"code": "10h"
+						},
+						{
+							"name": "11h",
+							"code": "11h"
+						},
+						{
+							"name": "Tarde",
+							"code": "Tarde"
+						},
+						{
+							"name": "12h",
+							"code": "12h"
+						},
+						{
+							"name": "13h",
+							"code": "13h"
+						},
+						{
+							"name": "14h",
+							"code": "14h"
+						},
+						{
+							"name": "15h",
+							"code": "15h"
+						},
+						{
+							"name": "16h",
+							"code": "16h"
+						}
+					]
+			}
+		},
+		handleURLformat(url) {
+			if (url == null || url == "" || url == undefined) {
+				return ""
+			}
+			else {
+				return url[0]
+			}
+		},
+		handleItensFormat(itens) {
+			if (itens == null || itens == "" || itens == undefined) {
+				return ""
+			}
+			else {
+				return itens.join('\n')
+			}
+		},
+	
+		async renderOSMotherInfo() {
+			if (appsmith.store.selectedOS["OS (Mãe)"] != undefined) {
+				let relativeOS = await Leitura_OS_Por_RecordID.run({
+					recordId: appsmith.store.selectedOS["OS (Mãe)"][0]
+				});
+			
+				let fields = relativeOS.fields
+
+				return fields["id_os"]
+			}
+			else{
+				return ""
+			}
+		},
+	
+		async renderOSChildInfo() {
+				if (appsmith.store.selectedOS["OS (Filha)"] != undefined) {
+				let relativeOS = await Leitura_OS_Por_RecordID.run({
+					recordId: appsmith.store.selectedOS["OS (Filha)"][0]
+				});
+			
+				let fields = relativeOS.fields
+
+				return fields["id_os"]
+			}
+			else {
+				return ""
+			}
+	}
+}
+
+
+	
+	
+	
+	
